22e8ef0fe5cbe366e81f3a040d8eb114c097e493
Gabriele Melendugno <gab.carpediem@gmail.com>
2024-05-21 21:22:44 +0200

    update assets paths automatically with js

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,2 +1,6 @@
-export const MEDIA_BASE_PATH = './assets/photographers/'; 
-export const PORTRAIT_BASE_PATH = './assets/photographers/Photographers-ID-Photos/'
+// export const MEDIA_BASE_PATH = './assets/photographers/'; 
+// export const PORTRAIT_BASE_PATH = './assets/photographers/Photographers-ID-Photos/'
+
+const BASE_URL = window.location.pathname.includes('repository-name') ? '/repository-name/' : '/';
+export const MEDIA_BASE_PATH = `${BASE_URL}assets/photographers/`; 
+export const PORTRAIT_BASE_PATH = `${BASE_URL}assets/photographers/Photographers-ID-Photos/`;
