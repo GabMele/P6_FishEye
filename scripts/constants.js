b84723383afb7af43c676d83960ebfe885553882
Gabriele Melendugno <gab.carpediem@gmail.com>
2024-05-21 21:29:07 +0200

    update mistake

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,6 +1,6 @@
-// export const MEDIA_BASE_PATH = './assets/photographers/'; 
-// export const PORTRAIT_BASE_PATH = './assets/photographers/Photographers-ID-Photos/'
+// export const MEDIA_BASE_PATH = '/assets/photographers/'; 
+// export const PORTRAIT_BASE_PATH = '/assets/photographers/Photographers-ID-Photos/'
 
-const BASE_URL = window.location.pathname.includes('repository-name') ? '/repository-name/' : '/';
+const BASE_URL = window.location.pathname.includes('P6_FishEye') ? '/P6_FishEye/' : '/';
 export const MEDIA_BASE_PATH = `${BASE_URL}assets/photographers/`; 
 export const PORTRAIT_BASE_PATH = `${BASE_URL}assets/photographers/Photographers-ID-Photos/`;
