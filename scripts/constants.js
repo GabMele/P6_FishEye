c8c76855bab0d9013aca4c2547abc868c5716d2b
Gabriele Melendugno <gab.carpediem@gmail.com>
2024-05-21 21:39:18 +0200

    update base url, other way

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,6 +1,17 @@
 // export const MEDIA_BASE_PATH = '/assets/photographers/'; 
 // export const PORTRAIT_BASE_PATH = '/assets/photographers/Photographers-ID-Photos/'
 
-const BASE_URL = window.location.pathname.includes('P6_FishEye') ? '/P6_FishEye/' : '/';
+// const BASE_URL = window.location.pathname.includes('P6_FishEye') ? '/P6_FishEye/' : '/';
+// export const MEDIA_BASE_PATH = `${BASE_URL}assets/photographers/`; 
+// export const PORTRAIT_BASE_PATH = `${BASE_URL}assets/photographers/Photographers-ID-Photos/`;
+
+
+const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+const BASE_URL = isLocal ? '/' : '/P6_FishEye/';
+
 export const MEDIA_BASE_PATH = `${BASE_URL}assets/photographers/`; 
 export const PORTRAIT_BASE_PATH = `${BASE_URL}assets/photographers/Photographers-ID-Photos/`;
+
+console.log("window.location.hostname :" + window.location.hostname);
+console.log("BASE_URL : " + BASE_URL);
+console.log("MEDIA_BASE_PATH : " + MEDIA_BASE_PATH);
