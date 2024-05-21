588154f067caf61e26065353ca5954fa2262bccb
Gabriele Melendugno <gab.carpediem@gmail.com>
2024-05-21 21:18:50 +0200

    update assets paths

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,2 +1,2 @@
-export const MEDIA_BASE_PATH = 'assets/photographers/'; 
-export const PORTRAIT_BASE_PATH = 'assets/photographers/Photographers-ID-Photos/'
+export const MEDIA_BASE_PATH = './assets/photographers/'; 
+export const PORTRAIT_BASE_PATH = './assets/photographers/Photographers-ID-Photos/'
