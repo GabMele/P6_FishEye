7e1506bd4f9d347a45fb262bd4cdcedd65444e47
Gabriele Melendugno <gab.carpediem@gmail.com>
2024-05-27 15:15:40 +0200

    added JSDocs on api.js

--- scripts/api/api.js
+++ scripts/api/api.js
@@ -1,26 +1,16 @@
-// // export default class Api {
-// //     constructor(url){
-// //         this.url = url;
-// //     }
-// export async function fetchData(url){
-//         try{
-//             const response = await fetch(this.url);
-//             const data = await response.json();
-//             return data;
-//         } catch(err){
-//             throw new Error(err);
-//         }
-//     }
-
-// //};
-
-
-export async function fetchData(url){
-    try{
+/**
+ * Fetches data from a given URL (in this project, "url" is actually a local path) and returns it as JSON.
+ *
+ * @param {string} url - The URL to fetch data from.
+ * @returns {Promise<Object>} A promise that resolves with the fetched data as a JavaScript object.
+ * @throws Will throw an error if the fetch request fails.
+ */
+export async function fetchData(url) {
+    try {
         const response = await fetch(url);
         const data = await response.json();
         return data;
-    } catch(err){
+    } catch (err) {
         throw new Error(err);
     }
 }
